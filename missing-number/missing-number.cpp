2a6968c110b099fb68998ef1b5d081afa9e1f455
Dhruv Bansal <dhruvbansal237@gmail.com>
2021-10-12 19:06:45 +0530

    Time: 52 ms (5.12%), Space: 27.2 MB (5.89%) - LeetHub

--- missing-number/missing-number.cpp
+++ missing-number/missing-number.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // vector<int> hash_table(nums.size()+1);
-        // for(int i=0;i<nums.size();++i)
-        //     hash_table[nums[i]]++;
-        // int res=0;
-        // for(int i=1;i<hash_table.size();++i){
-        //     if(hash_table[i]==0){
-        //         res=i;
-        //         break;
-        //     }
-        // }
-        // return res;
-        
-        //now we are using Cyclic Sort...
-//         int i=0,n=nums.size();
-//         while(i<n){
-//             int tmp=nums[i];
-//             if(nums[i]!=nums[tmp]&&nums[i]<n)
-//                 swap(nums[i],nums[tmp]);
-//             else
-//                 i++;
-//         }
-//         for(int index=0;index<n;++index)
-//         if(nums[index]!=index)
-//         return index;
-        
-//         return nums.size();
-        int ans;
-    for(int i=0;i<nums.size();){
-        if(i==nums[i] || nums[i] >= nums.size()){
-            i++;
-            continue;
-        }
-        else{
-            swap(nums[i], nums[nums[i]]);
-        }
-    }
-    for(int i=0;i<nums.size();i++){
-        if(i != nums[i]){
-            ans = i;
-        }
-    }
-    return ans;
+        int num=nums.size();
+        map<int,int> mp;
+        for(auto i:nums)
+            mp[i]++;
+        vector<int> v;
+        for(auto i:mp)
+            v.push_back(i.first);
+        for(int i=0;i<v.size();++i)
+            if(v[i]!=i)
+                return i;
+        return num;
     }
 };
