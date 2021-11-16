e68cd3fdb3744c0d8b0ea861d1fc7d53e515b3d2
Dhruv Bansal <dhruvbansal237@gmail.com>
2021-11-16 22:44:08 +0530

    Time: 4 ms (71.30%), Space: 11.8 MB (28.95%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1,mid;
-        while(low<=high){
-            mid=(low+high)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>=nums[low]){
-                if(nums[mid]>=target and nums[low]<=target)
-                high=mid-1;
-                else
-                low=mid+1;
-            }
-            else{
-                if(nums[mid]<=target and nums[high]>=target)
-                low=mid+1;
-                else
-                high=mid-1;
-            }
-        }
-        return -1;
+    int countRotations(vector<int> arr, int low, int high){
+	if (high < low)
+		return 0;
+
+	if (high == low)
+		return low;
+
+	int mid = low + (high - low)/2; 
+
+	if (mid < high && arr[mid+1] < arr[mid])
+	return mid+1;
+
+	if (mid > low && arr[mid] < arr[mid - 1])
+	return mid;
+
+	if (arr[high] > arr[mid])
+	return countRotations(arr, low, mid-1);
+	return countRotations(arr, mid+1, high);
+}
+int search(vector<int>& nums, int target) {
+    int res=-1;
+    int pos=countRotations(nums,0,nums.size()-1);
+  int low=0,high=pos-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target)
+        return mid;
+        else if(nums[mid]>target)
+        high=mid-1;
+        else
+        low=mid+1;
+    }
+    low=pos;
+    high=nums.size()-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target)
+        return mid;
+        else if(nums[mid]>target)
+        high=mid-1;
+        else
+        low=mid+1;
+    }
+    return res;
     }
 };
