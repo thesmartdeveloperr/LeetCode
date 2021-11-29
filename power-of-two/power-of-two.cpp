69db09a447428b5392a8087e91ddc961ac4d733d
Dhruv Bansal <dhruvbansal237@gmail.com>
2021-11-29 10:12:58 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (88.62%) - LeetHub

--- power-of-two/power-of-two.cpp
+++ power-of-two/power-of-two.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && !(n&(n-1));
+        if(n<=0)
+            return false;
+        if(n==1)
+            return true;
+        while(n>1){
+            if(n%2!=0)
+                return false;
+            n/=2;
+        }
+        return true;
     }
 };
