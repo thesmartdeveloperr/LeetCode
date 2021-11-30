e6a4f83d542077b0032735650826d2c333be38c2
Dhruv Bansal <dhruvbansal237@gmail.com>
2021-11-30 23:27:53 +0530

    Time: 0 ms (100.00%), Space: 13.7 MB (17.43%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,30 +3,12 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> res;
         int pos_first=-1,pos_last=-1;
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]<target)
-                low=mid+1;
-            else{
-                if(nums[mid]==target)
-                    pos_first=mid;
-                high=mid-1;
-            }
-        }
-        low=0;
-        high=nums.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]>target)
-                high=mid-1;
-            else{
-                if(nums[mid]==target){
-                    pos_last=mid;
-                }
-                low=mid+1;
-            }
-        }
+        int one=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int two=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(one<nums.size() and nums[one]==target)
+            pos_first=one;
+        if(two>0 and nums[two-1]==target)
+            pos_last=two-1;
         res.push_back(pos_first);
         res.push_back(pos_last);
         return res;
