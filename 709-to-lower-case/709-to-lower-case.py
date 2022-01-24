640bc1c4ecc679c4c9666c0f30513ff2744c810b
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-01-24 13:23:05 +0530

    Time: 24 ms (96.58%), Space: 14.2 MB (66.49%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -2,7 +2,7 @@ class Solution:
     def toLowerCase(self, s: str) -> str:
         lis=[]
         for i in s:
-            if i.upper():
+            if i.isupper():
                 lis.append(i.lower())
             else:
                 lis.append(i)
