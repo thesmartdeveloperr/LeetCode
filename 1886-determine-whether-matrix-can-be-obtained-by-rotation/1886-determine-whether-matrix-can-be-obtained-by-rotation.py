05d545f1b1f94f56d7efbb987d312d0a1f0fcdd2
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-02-04 13:53:49 +0530

    Time: 48 ms (61.82%), Space: 14 MB (91.61%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        def transpose(mat,n,target,k):
+            if k==3:
+                return False
+            mat.reverse()
+            for i in range(n):
+                for j in range(i,n):
+                    mat[i][j],mat[j][i]=mat[j][i],mat[i][j]
+            print(mat)    
+            if mat==target:
+                return True
+            else:
+                return transpose(mat,n,target,k+1)
+        n=len(mat)
+        k=0
+        if mat==target:
+            return True
+        return (transpose(mat,n,target,k))
