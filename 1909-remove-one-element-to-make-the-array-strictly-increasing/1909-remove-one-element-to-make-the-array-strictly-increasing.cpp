5c78f40dc9b2af16726d7fa4273d789edb88bbe0
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-02-04 18:28:26 +0530

    Time: 148 ms (5.27%), Space: 10.2 MB (14.66%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
+        // int n=nums.size();
+        // int count=0;
+        // stack<int> st;
+        // for(int i=0;i<n-1;i++){
+        //     if(!st.empty() and st.top()>=nums[i] and count==1) 
+        //         return false;
+        //     if(st.empty() or st.top()<nums[i])
+        //     st.push(nums[i]);
+        //     if(nums[i]<=nums[i-1])
+        //         count++;
+        // }
+        // return true;
         int n=nums.size();
         int count=0;
-        for(int i=0;i<n-1;i++){
-            int j=i+1;
-            if(nums[i]>=nums[j])
-                count++;
-            if(i>0 && nums[i-1] >= nums[j] && j<n-1 && nums[i]>=nums[j+1])
+        vector<int> res;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n;++j)
+                if(j!=i)
+                    res.push_back(nums[j]);
+            count=1;
+            for(int k=0;k<res.size()-1;++k)
+                if(res[k]<res[k+1])
                     count++;
-            if(count>1) 
-                return false;
+            if(count==res.size())
+                return true;
+            res.clear();
         }
-        return true;
+        return false;
     }
 };
