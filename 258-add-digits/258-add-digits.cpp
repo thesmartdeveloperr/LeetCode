d49aaca0beb18239455109343e70f242ce5fb47b
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-02-08 23:13:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.37%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int res;
+        if(num<10)
+            return num;
+        int sum=0;
+        while(num){
+            sum+=num%10;
+            num/=10;
+        }
+        num=sum;
+        return addDigits(num);
+    }
+};
