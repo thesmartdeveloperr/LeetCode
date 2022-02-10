7a03d3e9870395882cb2861d96d7cd7b0851542a
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-02-10 23:28:18 +0530

    Time: 0 ms, Memory: 7.8 MB - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+    int f(vector<int> &nums,int i,vector<int>&dp){
+        if(i<0)
+            return 0;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int res=max(f(nums,i-1,dp),f(nums,i-2,dp)+nums[i]);
+        return dp[i]=res;
+    }
+    
     int rob(vector<int>& nums) {
-        int even=0,odd=0,n=nums.size();
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-                even+=nums[i];
-                even=max(even,odd);
-            }
-            else{
-                odd+=nums[i];
-                odd=max(even,odd);
-            }    
-        }  
-        return max(even,odd);
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        return f(nums,nums.size()-1,dp);
     }
 };
