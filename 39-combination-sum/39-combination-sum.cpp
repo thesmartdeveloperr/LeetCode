df4f5581f584f27bf9cc3cb063424617b4cdb7a3
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-02-17 18:35:10 +0530

    Time: 10 ms (62.45%), Space: 10.6 MB (96.55%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    void findCombinations(int i,vector<int> &tmp,vector<int> &candidates,int n,int target){
+    void combinations(int i,int n,vector<int> &candidates,vector<int> &tmp,vector<vector<int>> &res,int target){
         if(i==n){
             if(target==0){
                 res.push_back(tmp);
             }
             return;
         }
-        //picking the element
-       if(candidates[i]<=target){
+        if(candidates[i]<=target){
             tmp.push_back(candidates[i]);
-            findCombinations(i,tmp,candidates,n,target-candidates[i]);
-            tmp.pop_back();   
-       }
-        //not picking the element
-        findCombinations(i+1,tmp,candidates,n,target);
+    combinations(i,n,candidates,tmp,res,target-candidates[i]);              tmp.pop_back();
+        }
+        combinations(i+1,n,candidates,tmp,res,target);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> tmp;
-     findCombinations(0,tmp,candidates,candidates.size(),target);
+        vector<vector<int>> res;
+        vector<int> temp;
+        combinations(0,candidates.size(),candidates,temp,res,target);
         return res;
     }
 };
