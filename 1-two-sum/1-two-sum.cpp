dcd6a648a2d6511bacd0299581c2dc5a71b9af6d
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-02-26 00:47:08 +0530

    Time: 12 ms (85.29%), Space: 11 MB (29.09%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();++i){
+            if(mp.find(target-nums[i])!=mp.end()){
+                res.push_back(mp[target-nums[i]]);
+                res.push_back(i);
+                break;
+            }
+            mp[nums[i]]=i;
+        }
+        return res;
+    }
+};
