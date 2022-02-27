ca8fcbeb4adf4b948ab4d5157caa694ab1591c7f
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-02-27 21:40:52 +0530

    Time: 328 ms (89.47%), Space: 89 MB (31.58%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -3,21 +3,15 @@ public:
     long long numberOfTripsForGivenTime(vector<int>&a , long long int givenTime)
     {
         long long totalTrips = 0;
-        for(auto x : a){
-            // convert it to long long int 
-            // long long int val = x;
-			
-            totalTrips += (givenTime / x);
-        }
+        for(auto x : a)
+            totalTrips +=givenTime/x;
         return totalTrips;
     }
     long long minimumTime(vector<int>& arr ,  int totalTrips) {
         long long lowestTime = 1;
         long long highestTime = 1e14;
-        while(lowestTime<highestTime)
-        {
+        while(lowestTime<highestTime){
             long long mid = lowestTime + (highestTime-lowestTime)/2;
-            
             if(numberOfTripsForGivenTime(arr , mid) >= totalTrips)
                 highestTime = mid;
             else
