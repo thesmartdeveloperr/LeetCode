682200fce97ebf2647d3c38812343a0b7db23488
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-03-05 23:21:54 +0530

    Time: 7 ms (82.55%), Space: 11.5 MB (39.44%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -3,15 +3,14 @@ public:
     int deleteAndEarn(vector<int>& nums) {
         int n = 10001;
         vector<int> values(n, 0);
-        for (int num : nums)
-            values[num] += num;
-
-        int take = 0, skip = 0;
-        for (int i = 0; i < n; i++) {
-            int takei = skip + values[i];
-            int skipi = max(skip, take);
-            take = takei;
-            skip = skipi;
+        for (int i:nums)
+            values[i]+=i;
+        int take=0, skip=0;
+        for (int i=0;i<n;i++){
+            int t=skip+values[i];
+            int s=max(skip,take);
+            take=t;
+            skip=s;
         }
         return max(take, skip);
     }
