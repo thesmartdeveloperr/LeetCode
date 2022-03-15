ea91b86ec14b2af572b27d9bb6ce97ec78588c07
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-03-15 11:59:22 +0530

    Time: 276 ms (48.16%), Space: 102.2 MB (7.75%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long minimalKSum(vector<int>& nums, int k) {
         long long res=(long long)k*(k+1)/2;
-        set<int> st(begin(nums),end(nums));
+        set<int> st(nums.begin(),nums.end());
         for(auto i:st)
             if(i<=k)
                 res+=(++k)-i;
