f927ee7c588c741922b7cec63559a15a00b90045
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-03-23 14:09:57 +0530

    Time: 26 ms (9.92%), Space: 7.5 MB (8.20%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        map<int,int> mp1,mp2;
+        for(auto i:s)
+            mp1[i]++;
+        for(auto i:t)
+            mp2[i]++;
+        for(auto i:s)
+            if(mp1[i]!=mp2[i])
+                return false;
+        for(auto i:t)
+            if(mp1[i]!=mp2[i])
+                return false;
+        return true;
     }
 };
