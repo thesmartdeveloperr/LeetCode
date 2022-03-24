6ce5c363ac342faf0944082e91a2b07ac920eed2
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-03-24 13:45:30 +0530

    Time: 14 ms (53.22%), Space: 8.1 MB (15.10%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,6 +9,18 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        // if(head==NULL or head->next==NULL)
+        //     return false;
+        // ListNode *slow=head,*fast=head;
+        // while(fast->next and fast->next->next){
+        //     slow=slow->next;
+        //     fast=fast->next->next;
+        //     if(slow==fast)
+        //         return true;
+        // }
+        // return false;
+        
+        //tortoise and hare algorithm
         if(head==NULL or head->next==NULL)
             return false;
         ListNode *slow=head,*fast=head;
