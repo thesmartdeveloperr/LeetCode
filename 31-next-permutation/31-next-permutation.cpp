07dc2b61400687b385666082a0557f7a110f6190
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-03 08:48:56 +0530

    Time: 4 ms (73.50%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int k=0;
-        do{
-            if(k++ == 1)
-                break;
-        }while(next_permutation(nums.begin(),nums.end()));
+        int lastPeak=-1;
+        for(int i=1;i<nums.size();++i)
+            if(nums[i]>nums[i-1])
+                lastPeak=i;
+        if(lastPeak==-1){
+            //array is sorted in descending order;
+            for(int i=0;i<nums.size()/2;++i)
+                swap(nums[i],nums[nums.size()-1-i]);
+            return;
+        }
+        int ind=lastPeak;
+        for(int i=lastPeak;i<nums.size();++i)
+            if(nums[i]>nums[lastPeak-1] and nums[i]<nums[ind])
+                ind=i;
+        swap(nums[lastPeak-1],nums[ind]);
+        sort(nums.begin()+lastPeak,nums.end());
+        return;
     }
 };
