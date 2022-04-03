37e6678d038548adc928cb98765144452c9f3861
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-03 08:13:49 +0530

    Time: 3 ms (87.24%), Space: 12 MB (93.15%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -3,9 +3,8 @@ public:
     void nextPermutation(vector<int>& nums) {
         int k=0;
         do{
-            if(k==1)
+            if(k++ == 1)
                 break;
-            k++;
         }while(next_permutation(nums.begin(),nums.end()));
     }
 };
