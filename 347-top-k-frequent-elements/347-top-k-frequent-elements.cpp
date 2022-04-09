8681341283dc0770c0d8a378c24a28c538c973fc
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-09 09:53:43 +0530

    Time: 19 ms (67.06%), Space: 13.9 MB (15.94%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -5,12 +5,11 @@ public:
         map<int,int> mp;
         for(auto i:nums)
             mp[i]++;
-        vector<pair<int,int>> v;
+        priority_queue<pair<int,int>> v;
         for(auto i:mp)
-            v.push_back({i.second,i.first});
-        sort(v.begin(),v.end(),greater<pair<int,int>>());
+            v.push({i.second,i.first});
         for(int i=0;i<k;++i)
-            res.push_back(v[i].second);
+            res.push_back(v.top().second),v.pop();
         return res;
     }
 };
