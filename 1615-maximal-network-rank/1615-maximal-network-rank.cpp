1139e7ff045399be631f3b38646ceb90db68d2b5
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-09 13:46:38 +0530

    Time: 129 ms (43.83%), Space: 42.1 MB (14.77%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,21 +1,17 @@
-class Solution {
+class Solution{
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         int mx=0;
-        vector<vector<int>> adj_mat(n,vector<int>(n,0));
-        map<int,vector<int>> adj;
-        for(auto i:roads){
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
-            adj_mat[i[0]][i[1]]=1;
-            adj_mat[i[1]][i[0]]=1;
-        }
-        for(int i=0;i<adj.size();++i){
-            for(int j=i+1;j<adj.size();++j){
-                int connect=adj_mat[i][j]==1?1:0;
-                int val=adj[i].size()+adj[j].size()-connect;
-                mx=mx>val?mx:val;
-            }
+        vector<unordered_set<int>> adj(n);
+        for(auto i:roads)
+            adj[i[0]].insert(i[1]),adj[i[1]].insert(i[0]);   
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;++j){
+                int roads=adj[i].size()+adj[j].size();
+                if(adj[j].count(i))
+                    roads--;
+                mx=max(mx,roads);
+            }   
         }
         return mx;
     }
