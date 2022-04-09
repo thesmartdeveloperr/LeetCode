daad762c088ed3d31c3d2095ef18ee0cff6bca29
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-09 09:51:28 +0530

    Time: 26 ms (34.77%), Space: 13.6 MB (71.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -8,15 +8,8 @@ public:
         vector<pair<int,int>> v;
         for(auto i:mp)
             v.push_back({i.second,i.first});
-        sort(v.begin(),v.end());
-        for(auto i:v)
-            cout<<i.first<<","<<i.second<<endl;
-        if(k==v.size()){
-            for(int i=0;i<v.size();++i)
-                res.push_back(v[i].second);
-            return res;
-        }
-        for(int i=v.size()-1;i>=v.size()-k;--i)
+        sort(v.begin(),v.end(),greater<pair<int,int>>());
+        for(int i=0;i<k;++i)
             res.push_back(v[i].second);
         return res;
     }
