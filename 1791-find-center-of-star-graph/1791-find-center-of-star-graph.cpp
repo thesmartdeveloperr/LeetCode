f3f6dc97b02db79a1530d39c4040385bc1f9aaa0
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-09 11:28:04 +0530

    Time: 475 ms (7.57%), Space: 90.3 MB (10.91%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-    // logically the center will be the node that has the max. number of connections...
     int findCenter(vector<vector<int>>& edges) {
-        if(edges[0][0]==edges[1][0] or edges[0][0]==edges[1][1])
-            return edges[0][0];
-        else
-            return edges[0][1];
+        // map<int,vector<int>> adj;
+        vector<int> adj[100007];
+        for(auto i:edges){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        for(int i=0;i<100007;++i){
+            if(adj[i].size()==edges.size())
+                return i;
+        }
+        return -1;
     }
 };
