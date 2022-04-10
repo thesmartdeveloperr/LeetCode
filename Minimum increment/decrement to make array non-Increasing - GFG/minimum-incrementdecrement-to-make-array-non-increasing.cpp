635b331026312eb5960407b4bda8ae33edc8e387
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-10 19:19:58 +0530

    Added solution - LeetHub

--- Minimum increment/decrement to make array non-Increasing - GFG/minimum-incrementdecrement-to-make-array-non-increasing.cpp
+++ Minimum increment/decrement to make array non-Increasing - GFG/minimum-incrementdecrement-to-make-array-non-increasing.cpp
@@ -10,30 +10,18 @@ using namespace std;
 class Solution{
 public:
     int minOperations(int *a,int n){
-        int sum = 0;
+        int ops=0;
+        priority_queue<int,vector<int>,greater<int>> pq;
  
-    // min heap
-    priority_queue<int, vector<int>, greater<int> > pq;
- 
-    // Here in the loop we will
-    // check that whether the upcoming
-    // element of array is less than top
-    // of priority queue. If yes then we
-    // calculate the difference. After
-    // that we will remove that element
-    // and push the current element in
-    // queue. And the sum is incremented
-    // by the value of difference
-    for (int i = 0; i < n; i++) {
-        if (!pq.empty() && pq.top() < a[i]) {
-            sum += a[i]-pq.top();
+        for (int i=0;i<n;i++) {
+            if (!pq.empty() and pq.top()<a[i]){
+                ops+=a[i]-pq.top();
+                pq.push(a[i]);
+                pq.pop();
+            }
             pq.push(a[i]);
-            pq.pop();
         }
-        pq.push(a[i]);
-    }
- 
-    return sum;
+        return ops;
     }
 };
 
