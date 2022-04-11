d58db98fb5a2626d7715968be3d1530105cc7df9
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-11 22:46:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.55%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
+    int countSetBits(int x){
+        int count=0;
+        while(x){
+            count++;
+            x&=(x-1);
+        }
+        return count;
+    }
     int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start^goal);
+        // return __builtin_popcount(start^goal);
+        return countSetBits(start^goal);
     }
 };
