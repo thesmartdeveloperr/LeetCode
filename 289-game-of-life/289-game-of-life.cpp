f3a76199743688ad4d20c83e6d763da46230f6fa
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-12 18:28:49 +0530

    Time: 3 ms (45.24%), Space: 8.8 MB (5.20%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    void gameOfLife(vector<vector<int>>& board) {
+    int find_neighbours(vector<vector<int>> board,int i,int j){
         int n=board.size();
         int m=board[0].size();
         int nbr=0;
+        if(j>0 and board[i][j-1]==1)
+            nbr++;
+        if(j<m-1 and board[i][j+1]==1)
+            nbr++;
+        if(i>0 and board[i-1][j]==1)
+            nbr++;
+        if(i<n-1 and board[i+1][j]==1)
+            nbr++;
+        if(i>0 and j>0 and board[i-1][j-1]==1)
+            nbr++;
+        if(i>0 and j<m-1 and board[i-1][j+1]==1)
+            nbr++;
+        if(i<n-1 and j>0 and board[i+1][j-1]==1)
+            nbr++;
+        if(i<n-1 and j<m-1 and board[i+1][j+1]==1)
+            nbr++;
+        return nbr;
+    }
+    void gameOfLife(vector<vector<int>>& board) {
+        int n=board.size();
+        int m=board[0].size();
         vector<vector<int>> res(n,vector<int>(m,0));
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
-                nbr=0;
-                if(j>0 and board[i][j-1]==1)
-                    nbr++;
-                if(j<m-1 and board[i][j+1]==1)
-                    nbr++;
-                if(i>0 and board[i-1][j]==1)
-                    nbr++;
-                if(i<n-1 and board[i+1][j]==1)
-                    nbr++;
-                if(i>0 and j>0 and board[i-1][j-1]==1)
-                    nbr++;
-                if(i>0 and j<m-1 and board[i-1][j+1]==1)
-                    nbr++;
-                if(i<n-1 and j>0 and board[i+1][j-1]==1)
-                    nbr++;
-                if(i<n-1 and j<m-1 and board[i+1][j+1]==1)
-                    nbr++;
+                int nbr=find_neighbours(board,i,j);
                 if(board[i][j]==1){
                     if(nbr<2)
                         res[i][j]=0;
