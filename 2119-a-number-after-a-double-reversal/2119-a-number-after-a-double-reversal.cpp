e5d78dbb87e67ceefaf91894db78b4ac2128d59a
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-04-23 22:41:07 +0530

    Time: 5 ms (9.26%), Space: 5.8 MB (88.06%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -3,6 +3,8 @@ public:
     int reverse(int Num){
         int sum=0;
         while(Num){
+            if(sum>INT_MAX/10 or sum<INT_MIN/10)
+                return 0;
             sum = sum * 10 + Num % 10;
             Num /= 10;
         }
