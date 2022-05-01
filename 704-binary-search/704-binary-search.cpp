77f6b8dd698f75356c37db3693dcb83151c22038
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-05-01 14:54:22 +0530

    Time: 52 ms (44.89%), Space: 27.6 MB (12.99%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int pos=-1;
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[mid]<target)
-                low=mid+1;
-            else
-                high=mid-1;
-        }
-        return pos;
+        // int pos=-1;
+        // int low=0,high=nums.size()-1;
+        // while(low<=high){
+        //     int mid=low+(high-low)/2;
+        //     if(nums[mid]==target)
+        //         return mid;
+        //     else if(nums[mid]<target)
+        //         low=mid+1;
+        //     else
+        //         high=mid-1;
+        // }
+        // return pos;
+        /* cool method is to use binary_search algorithm from stl*/
+        int pos=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        return (pos<nums.size() and nums[pos]==target)?pos:-1;
     }
 };
