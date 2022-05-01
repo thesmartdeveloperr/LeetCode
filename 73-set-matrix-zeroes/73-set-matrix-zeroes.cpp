d16a6448fc224c4dae8e84d23d6e1ff5ed40c3f6
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-05-01 15:41:25 +0530

    Time: 16 ms (72.03%), Space: 13.2 MB (82.66%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        map<int,bool> mp1,mp2;
         int m=matrix.size();
         int n=matrix[0].size();
+        int col0=1; 
         for(int i=0;i<m;++i){
-            for(int j=0;j<n;++j){
+            if(matrix[i][0]==0)
+                col0=0;
+            for(int j=1;j<n;++j){
                 if(matrix[i][j]==0){
-                    mp1[i]=true;
-                    mp2[j]=true;
+                    matrix[i][0]=matrix[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;++i){
-            for(int j=0;j<n;++j){
-                if(mp1.find(i)!=mp1.end() or mp2.find(j)!=mp2.end())
+        for(int i=m-1;i>=0;--i){
+            for(int j=n-1;j>=1;--j){
+                if(matrix[i][0]==0 or matrix[0][j]==0)
                     matrix[i][j]=0;
             }
+            if(col0==0)
+                matrix[i][0]=0;
         }
     }
 };
