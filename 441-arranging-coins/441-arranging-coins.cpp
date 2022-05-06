a712477802fcf6b4e86c42ddbef9fb9db9462b33
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-05-06 18:04:24 +0530

    Time: 17 ms (13.83%), Space: 5.9 MB (28.83%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-      long long i=0;
-        int res=0;
-        long long prev=1;
-        while(i+prev<=n){
-            i+=prev;
-            prev++;
-            res++;
-        }
+      long long i=0,res=0,prev=1;
+        while(i+prev<=n)
+            i+=prev,prev++,res++;
         return res;
     }
 };
