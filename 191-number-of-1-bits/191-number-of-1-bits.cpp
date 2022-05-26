14a88dd47056d14a19892e4f64fe1e2724e88fe9
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-05-26 15:50:53 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
+        int res=0;
+        while(n){
+            res++;
+            n&=(n-1);
+        }
+        return res;
     }
 };
