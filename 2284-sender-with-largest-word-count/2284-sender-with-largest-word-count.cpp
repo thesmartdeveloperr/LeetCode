4659b94f34aaad912494ae7b6ab401b1896f580a
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-05-28 22:13:58 +0530

    Time: 600 ms (25.00%), Space: 83.3 MB (50.00%) - LeetHub

---
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int split(string s){
+        int res=0;
+        stringstream ss(s);
+        string word;
+        while(ss>>word){
+            res++;
+        }
+        return res;
+    }
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        string res="";
+        map<string,int> mp;
+        for(int i=0;i<senders.size();++i)
+            mp[senders[i]]+=split(messages[i]);
+        int words=0;
+        for(auto i:mp){
+            if(i.second>=words){
+                words=i.second;
+                if(res<i.first)
+                    res=i.first;
+            }
+        }
+        return res;
+    }
+};
