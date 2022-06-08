2ef00070cbf9e9024f63b2cdcb6291360f13879e
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-06-08 20:37:26 +0530

    Time: 2 ms (43.62%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    bool isPalindrome(string s){
-        int n=s.size();
-        for(int i=0;i<n;++i){
-            if(s[i]!=s[n-1-i])
-                return false;
-        }
-        return true;
-    }
     int removePalindromeSub(string s) {
-        return isPalindrome(s)?1:2;
+        return 2-(s==string(s.rbegin(),s.rend()))-s.empty();
     }
 };
