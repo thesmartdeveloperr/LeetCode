cd5116b1dda10d46874f27caf424f0b16c281c20
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-06-23 13:55:49 +0530

    Time: 437 ms (30.66%), Space: 63.4 MB (29.84%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
         priority_queue<int> pq;
-        for(auto &i:courses){
-            swap(i[0],i[1]);
-        }
-        sort(courses.begin(),courses.end());
+        sort(courses.begin(),courses.end(),[](vector<int> &a,vector<int>&b){
+            return a[1]<b[1];
+        });
         int sum=0;
         for(auto i:courses) {
-            sum+=i[1];
-            pq.push(i[1]);
-            if(sum>i[0]) {
+            sum+=i[0];
+            pq.push(i[0]);
+            if(sum>i[1]) {
                 sum-=pq.top();
                 pq.pop();
             }
