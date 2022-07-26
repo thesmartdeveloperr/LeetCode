f91d60c834b73a85d7f4955dfdf1baab1ad793aa
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-07-26 11:38:48 +0530

    Time: 2 ms (52.46%), Space: 6.4 MB (50.82%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -3,19 +3,18 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for(auto i:s){
-            if(i=='(' or i=='{' or i=='[')
+            if(st.size()==0 or i=='{' or i=='[' or i=='('){
                 st.push(i);
-            else{
-                if(!st.empty() and i==')' and st.top()=='(')
-                    st.pop();
-                else if(!st.empty() and i=='}' and st.top()=='{')
-                    st.pop();
-                else if(!st.empty() and i==']' and st.top()=='[')
-                    st.pop();
-                else
-                    st.push(i);
             }
+            else if(st.top()=='(' and i==')')
+                st.pop();
+            else if(st.top()=='{' and i=='}')
+                st.pop();
+            else if(st.top()=='[' and i==']')
+                st.pop();
+            else
+                return false;
         }
-        return st.empty();
+        return st.size()==0;
     }
 };
