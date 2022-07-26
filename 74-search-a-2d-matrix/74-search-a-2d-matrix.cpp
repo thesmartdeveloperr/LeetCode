bd4aa2753cd6a6b8a2dbedfdf2696986d78ead34
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-07-26 11:30:56 +0530

    Time: 6 ms (60.03%), Space: 9.4 MB (78.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,7 +4,7 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         int i=0,j=n-1;
-        while(i>=0 and i<m and j>=0 and j<n){
+        while(i<m and j>=0){
             if(matrix[i][j]>target)
                 j--;
             else if(matrix[i][j]<target)
