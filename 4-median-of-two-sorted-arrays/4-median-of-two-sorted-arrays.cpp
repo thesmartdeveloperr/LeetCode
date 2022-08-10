c29ccd5b3da519ccb057124cb799cc34350256a6
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-08-10 15:38:51 +0530

    Time: 35 ms (96.34%), Space: 89.3 MB (61.24%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
+    /*
+        Time: O(log(min(n,m)))
+        Space: O(1)
+    */
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double res;
+        if(nums1.size()>nums2.size())
+            return findMedianSortedArrays(nums2,nums1);
         int n=nums1.size(),m=nums2.size();
-        vector<int> sorted;
-        int i=0,j=0;
-        while(i<n and j<m){
-            if(nums1[i]<=nums2[j]){
-                sorted.push_back(nums1[i]);
-                i++;
+        int low=0,high=n;
+        while(low<=high){
+            int cut1=(low+high)/2;
+            int cut2=(n+m+1)/2-cut1;
+            int left1=(cut1==0)?INT_MIN:nums1[cut1-1];
+            int right1=(cut1>=n)?INT_MAX:nums1[cut1];
+            int left2=(cut2==0)?INT_MIN:nums2[cut2-1];
+            int right2=(cut2>=m)?INT_MAX:nums2[cut2];
+            if(left1<=right2 and left2<=right1){
+                if((n+m)%2==0)
+                    return (max(left1,left2)+min(right1,right2))/2.0;
+                else
+                    return max(left1,left2);
             }
-            else{
-                sorted.push_back(nums2[j]);
-                j++;
-            }
+            else if(left1>right2)
+                high=cut1-1;
+            else
+                low=cut1+1;
         }
-        while(i<n){
-            sorted.push_back(nums1[i++]);
-        }
-        while(j<m){
-            sorted.push_back(nums2[j++]);
-        }
-        if(sorted.size()&1){
-            return (double)sorted[sorted.size()/2];
-        }
-        return (double)(sorted[(sorted.size()-1)/2]+sorted[sorted.size()/2])/2;
+        return 0.0;
     }
 };
