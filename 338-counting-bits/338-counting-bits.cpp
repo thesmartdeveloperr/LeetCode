578c52233324062b05c9b1e80f430c610b568dc1
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-08-14 11:17:17 +0530

    Time: 4 ms (88.61%), Space: 7.8 MB (72.21%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res;
-        for(int i=0;i<=n;++i){
-            res.push_back(__builtin_popcount(i));
-        }
+        vector<int> res(n+1);
+        for(int i=1;i<=n;++i)
+            res[i]=res[i/2]+i%2;
         return res;
     }
 };
