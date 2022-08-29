ef10e88cc9717de42f470cf5128bb0e829f5d960
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-08-29 15:19:08 +0530

    Time: 79 ms (19.02%), Space: 12.3 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,vector<vector<int>>& visited,int i,int j){
-        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or visited[i][j] or grid[i][j]=='0')
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or grid[i][j]=='0')
             return;
-        visited[i][j]=1;
-        dfs(grid,visited,i+1,j);
-        dfs(grid,visited,i-1,j);
-        dfs(grid,visited,i,j+1);
-        dfs(grid,visited,i,j-1);
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
     int numIslands(vector<vector<char>>& grid) {
         int res=0;
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> visited(m,vector<int>(n,0));
         for(int i=0;i<m;++i){
             for(int j=0;j<n;++j){
-                if(grid[i][j]=='1' and !visited[i][j]){
+                if(grid[i][j]=='1'){
                     res++;
-                    dfs(grid,visited,i,j);
+                    dfs(grid,i,j);
                 }
             }
         }
