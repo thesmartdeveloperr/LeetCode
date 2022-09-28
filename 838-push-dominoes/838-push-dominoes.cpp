2d1e5ceb413fb3e3ccf9b1bef6be1cc37ee720a1
Dhruv Bansal <dhruvbansal237@gmail.com>
2022-09-28 14:37:55 +0530

    Time: 175 ms (9.97%), Space: 22.8 MB (14.59%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    string pushDominoes(string d) {
-        d = 'L' + d + 'R';
-        string res = "";
-        for (int i = 0, j = 1; j < d.length(); ++j) {
-            if (d[j] == '.') continue;
-            int middle = j - i - 1;
-            if (i > 0)
-                res += d[i];
-            if (d[i] == d[j])
-                res += string(middle, d[i]);
-            else if (d[i] == 'L' && d[j] == 'R')
-                res += string(middle, '.');
+    string pushDominoes(string dominoes) {
+        string res="";
+        int n=dominoes.size();
+        vector<int> l2r(n,INT_MAX),r2l(n,INT_MAX);
+        // left->right
+        bool fl=0;
+        for(int i=0;i<n;++i){
+            if(dominoes[i]=='R')
+                l2r[i]=0,fl=1;
+            else if(fl and i>0 and dominoes[i]=='.')
+                l2r[i]=l2r[i-1]+1;
             else
-                res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
-            i = j;
+                fl=0;
+        }
+        //right->left
+        fl=0;
+        for(int i=n-1;i>=0;--i){
+            if(dominoes[i]=='L')
+                r2l[i]=0,fl=1;
+            else if(fl and i<n and dominoes[i]=='.')
+                r2l[i]=r2l[i+1]+1;
+            else
+                fl=0;
+        }
+        for(int i=0;i<n;++i){
+            if(l2r[i]==r2l[i])
+                res+=".";
+            else if(l2r[i]<r2l[i])
+                res+="R";
+            else
+                res+="L";
         }
         return res;
     }
